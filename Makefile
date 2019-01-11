ef297317a0f159c27b41c959793b523bd3d9bbe4
tonykyo3232 <38056326+tonykyo3232@users.noreply.github.com>
2019-01-11 11:26:35 -0800

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------------------
+#  
+#  Makefile for CMPS 112 Lab0
+#  Compiles all .java files in the current directory and creates an executable
+#  jar file called Sparse. 
+#
+#------------------------------------------------------------------------------ 
+
+all:    
+	javac helloWorld.java
+	java helloWorld 
---
+++ helloWorld.java
@@ -0,0 +1,20 @@
+/********************
+ Hello World program
+ This program will show how to print the message from languages of 
+ C, C++, Java and Python
+********************/
+public class helloWorld {
+	public static void main(String[] args) {
+	    System.out.println("***************************");
+	    System.out.println("Hi there!");
+	    System.out.println("I'm doing this project with Java");
+	    System.out.println("So far, I have learn C, C++, Java, Python");
+	    System.out.println("Rencently, I will be learning Scheme");
+	    System.out.println("In each language of saying 'Hello World': ");
+	    System.out.println("Python: print('Hello World')");
+	    System.out.println("C++: cout << 'Hello World' << endl;");
+	    System.out.println("C: printf('Hello WOrld');");
+	    System.out.println("Java: System.out.println('Hello World');");
+	    System.out.println("***************************");
+	}
+}
